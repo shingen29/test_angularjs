dbe5976e7cb2a75dcbef5ce3f4867cf0ec276d97
Makoto Otsuka <mametra2580@gmail.com>
2017-10-16 16:18:30 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// app.js
+// [myAppというAngularJSのアプリケーションを作ります]宣言だと思ってください。
+
+angular.module('myApp', [])
+
+  .controller('AppController', function($scope) {
+    // 入力されたユーザー名を保持
+    $scope.username = '';
+    // ユーザー一覧（空の配列で初期化）
+    $scope.users = [];
+
+    // 登録を押されたときの関数
+    $scope.submit = function() {
+      // 新しいユーザー一覧に登録する
+      $scope.users.push({
+        username: $scope.username,
+        url: '//twitter.com/' + $scope.username
+      });
+    };
+  });
